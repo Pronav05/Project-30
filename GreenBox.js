29caf51087bba972742d31698a0f5e244b2c64b1
Pronav05 <pforpranav05@gmail.com>
2020-11-19 17:46:14 +0800

    tower siege

--- GreenBox.js
+++ GreenBox.js
@@ -12,7 +12,7 @@ class GreenBox extends Box{
         World.remove(world, this.body);
         push();
         this.Visiblity = this.Visiblity - 5;
-        tint(255,this.Visiblity);
+        //tint(255,this.Visiblity);
         rect(this.x,this.y, 20, 30);
         pop();
       }
