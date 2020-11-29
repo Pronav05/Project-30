1343765c14a70d93b47fc4e0bdf297c5fb4169cf
Pronav05 <pforpranav05@gmail.com>
2020-11-29 11:53:23 +0800

    added images

--- Box.js
+++ Box.js
@@ -8,12 +8,13 @@ class Box{
         this.body = Bodies.rectangle(x,y,width,height,options);
         this.width = width;
         this.height = height;
+        this.image = loadImage("images/enemy.png");
         World.add(world,this.body);
     }
     display(){
         var pos = this.body.position;
-        rectMode(CENTER);
-        rect(pos.x,pos.y,this.width,this.height);
+        imageMode(CENTER);
+        image(this.image, this.body.position.x,this.body.position.y,this.width,this.height);
         fill("white");
     }
     
--- GreenBox.js
+++ GreenBox.js
@@ -12,8 +12,8 @@ class GreenBox extends Box{
         World.remove(world, this.body);
         push();
         this.Visiblity = this.Visiblity - 5;
-        //tint(255,this.Visiblity);
-        rect(this.x,this.y, 20, 30);
+        tint(255,this.Visiblity);
+        image(this.body.position.x,this.body.position.y, 20, 30);
         pop();
       }
     }
